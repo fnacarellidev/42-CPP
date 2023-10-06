c24a989c19b5e0266d0c2f2b83bbaa7d326f3d6b
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-10-06 06:04:21 -0300

    style: removed whitespaces

--- cpp01/ex00/Zombie.hpp
+++ cpp01/ex00/Zombie.hpp
@@ -5,8 +5,8 @@ class Zombie {
 	private:
 		std::string name;
 	public:
-		Zombie	(void);
-		Zombie	(std::string name);
+		Zombie(void);
+		Zombie(std::string name);
 		~Zombie();
 		void	announce();
 };
