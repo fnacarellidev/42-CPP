85d9a4f1b87a2a8a1358756f66b1ae44f8147a52
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-10-17 19:16:20 -0300

    cpp01 ex05 added pragma once and formated messages

--- cpp01/ex05/Harl.hpp
+++ cpp01/ex05/Harl.hpp
@@ -1,8 +1,12 @@
+#pragma once
 #include <iostream>
-#define DEBUG_MSG "[DEBUG] DEBUGGING"
-#define INFO_MSG "[INFO] INFOS"
-#define WARN_MSG "[WARNING] WARNINGS"
-#define ERR_MSG "[ERROR] ERRORS"
+#define DEBUG_MSG "[  DEBUG  ] I love having extra bacon for my " \
+		"7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!"
+#define INFO_MSG "[   INFO  ] I cannot believe adding extra bacon costs more money." \
+		"You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
+#define WARN_MSG "[ WARNING ] I think I deserve to have some extra bacon for free. I’ve been coming for" \
+		"years whereas you started working here since last month."
+#define ERR_MSG "[  ERROR  ] This is unacceptable! I want to speak to the manager now."
 
 class Harl {
 	private:
