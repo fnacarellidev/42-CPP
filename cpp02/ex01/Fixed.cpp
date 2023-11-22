e2fdbddb45ba36795c96d7d32bd67da98a087c77
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-11-22 17:03:10 -0300

    feat: cpp02 ex01 solved

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -1,13 +1,51 @@
 #include "Fixed.hpp"
 
-Fixed::Fixed(const int x) {
+Fixed::Fixed(): _rawBits(0) {
+	std::cout << "Default constructor called" << std::endl;
 
 }
 
-Fixed::Fixed(const float x){
-	_rawBits = round(x * (1 << _fractionalBits));
+Fixed::Fixed(Fixed const &obj)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	if (this != &obj)
+		*this = obj;
+}
+
+Fixed::Fixed(const int x) {
+	std::cout << "Int constructor called" << std::endl;
+	_rawBits = x * (1 << _fractionalBits);
+}
+
+Fixed::Fixed(const float x) {
+	std::cout << "Float constructor called" << std::endl;
+	_rawBits = roundf(x * (1 << _fractionalBits));
 }
 
 Fixed::~Fixed() {
-	std::cout << "Destructor operator called" << std::endl;
+	std::cout << "Destructor called" << std::endl;
+}
+
+int	Fixed::getRawBits() const {
+	return _rawBits;
+}
+
+Fixed	&Fixed::operator=(const Fixed &copy) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &copy)
+		_rawBits = copy.getRawBits();
+	return (*this);
+}
+
+int		Fixed::toInt() const {
+	return _rawBits / (1 << _fractionalBits);
+}
+
+float	Fixed::toFloat() const {
+	return _rawBits / ((float) (1 << _fractionalBits));
+}
+
+std::ostream	&operator<<(std::ostream &out, const Fixed &fixed) {
+	out << fixed.toFloat();
+	return out;
 }
--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -7,10 +7,15 @@ class Fixed {
 		int					_rawBits;
 		static const int	_fractionalBits = 8;
 	public:
+		Fixed();
 		Fixed(const int x);
 		Fixed(const float x);
+		Fixed(Fixed const &copy);
 		~Fixed();
-		Fixed & operator = (const Fixed &rhs);
+		Fixed & operator = (const Fixed &copy);
 		int		getRawBits() const;
 		void	setRawBits(int const raw);
+		int		toInt() const;
+		float	toFloat() const;
+		friend	std::ostream	&operator<<(std::ostream &out, const Fixed &fixed);
 };
