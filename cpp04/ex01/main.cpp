a6af57f99dfdcab1a52ad32b06850c5a40ce5d1e
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-12-07 17:39:34 -0300

    feat: reinsert tests

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -4,20 +4,21 @@
 
 int main() {
 	Dog		*new_doggo = new Dog();
-	/* Animal **animals = new Animal*[100]; */
+	Animal **animals = new Animal*[100];
 
-	/* for (int i = 0; i < 50; ++i) */
-	/* 	animals[i] = new Cat(); */
-	/* for (int i = 50; i < 100; ++i) */
-	/* 	animals[i] = new Dog(); */
+	for (int i = 0; i < 50; ++i)
+		animals[i] = new Cat();
+	for (int i = 50; i < 100; ++i)
+		animals[i] = new Dog();
 
-	/* for (int i = 0; i < 100; ++i) { */
-	/* 	std::cout << animals[i]->getType() << std::endl; */
-	/* 	delete animals[i]; */
-	/* } */
+	for (int i = 0; i < 100; ++i) {
+		std::cout << animals[i]->getType() << std::endl;
+		delete animals[i];
+	}
 
 	new_doggo->setIdea(0, "chimkem numgets");
 	std::cout << new_doggo->getIdea(0) << std::endl;
 	delete new_doggo;
+	delete[] animals;
 	return (0);
 }
