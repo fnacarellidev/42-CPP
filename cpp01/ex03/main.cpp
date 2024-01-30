996f621f56fe83d89249299ed0d33bf9f125b98a
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-01-30 16:07:33 -0300

    add some tests

--- cpp01/ex03/main.cpp
+++ cpp01/ex03/main.cpp
@@ -4,6 +4,7 @@
 
 int main() {
 	{
+		std::cout << "TEST 0" << std::endl;
 		Weapon club = Weapon("crude spiked club");
 		HumanA bob("Bob", club);
 		bob.attack();
@@ -13,13 +14,29 @@ int main() {
 		bob.attack();
 	}
 	{
+		std::cout << "TEST 1" << std::endl;
 		Weapon club = Weapon("crude spiked club");
 		HumanB jim("Jim");
-		jim.setWeapon(&club);
+		jim.setWeapon(club);
 		jim.attack();
 		club.setType("some other type of club");
 		jim.attack();
 		club.setType("");
+		jim.attack();
+	}
+	{
+		std::cout << "TEST 2" << std::endl;
+		Weapon	sword = Weapon("Long Sword");
+		HumanA	bob("Bob", sword);
+		bob.attack();
+		sword.setType("Large sword");
+		bob.attack();
+
+		Weapon	bow = Weapon("Short Bow");
+		HumanB	jim("Jim");
+		jim.setWeapon(bow);
+		jim.attack();
+		bow.setType("Long Bow");
 		jim.attack();
 	}
 	return 0;
