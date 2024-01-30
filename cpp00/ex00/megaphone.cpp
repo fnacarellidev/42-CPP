5c71198afc164c9aed23a409ca25967d517a145e
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-01-30 11:07:58 -0300

    refactor: remove class implementation, only functions

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 
-class Megaphone {
-	public:
-		static void scream(void) {
-			std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		};
-		static void scream(int argc, char **argv) {
-			for (int i = 1; i < argc; ++i) {
-				std::string arg = argv[i];
-				for (size_t j = 0; j < arg.length(); ++j) {
-					std::cout << char(toupper(arg[j]));
-				}
-			}
-			std::cout << "\n";
-		};
+static void scream(void) {
+	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+};
+
+static void scream(int argc, char **argv) {
+	for (int i = 1; i < argc; ++i) {
+		std::string arg = argv[i];
+		for (size_t j = 0; j < arg.length(); ++j)
+			std::cout << char(toupper(arg[j]));
+	}
+	std::cout << "\n";
 };
 
 int main(int argc, char **argv) {
-	argc < 2 ? Megaphone::scream() : Megaphone::scream(argc, argv);
+	argc < 2 ? scream() : scream(argc, argv);
 	return 0;
 }
