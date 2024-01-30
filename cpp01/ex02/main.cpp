fb99c513b5beadb9a362a800b9f8e2781cc0b767
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-01-30 15:57:47 -0300

    fix: renaming variable just to make sure

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -2,16 +2,16 @@
 
 int main() {
 	std::string		str = "HI THIS IS BRAIN";
-	std::string*	strPtr = &str;
-	std::string&	strRef = str;
+	std::string*	stringPTR = &str;
+	std::string&	stringREF = str;
 
 	std::cout << "Address of str: " << &str << std::endl;
-	std::cout << "Address of strPtr: " << strPtr << std::endl;
-	std::cout << "Address of strRef: " << &strRef << std::endl;
+	std::cout << "Address of stringPTR: " << stringPTR << std::endl;
+	std::cout << "Address of stringREF: " << &stringREF << std::endl;
 
 	std::cout << '\n';
 
 	std::cout << "Content of str: " << str << std::endl;
-	std::cout << "Content of strPtr: " << *strPtr << std::endl;
-	std::cout << "Content of strRef: " << strRef << std::endl;
+	std::cout << "Content of stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Content of stringREF: " << stringREF << std::endl;
 }
