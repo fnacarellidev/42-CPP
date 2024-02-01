a9b91a781de7ead29e1771ffabe05456f0d87dc0
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-02-01 18:36:47 -0300

    add some tests cpp04 ex00

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -6,20 +6,42 @@
 
 int main() {
 	{
-		std::cout << "\nSUBJECT-TEST" << std::endl;
+		std::cout << "SUBJECT TEST" << std::endl;
 		const Animal	*meta = new Animal();
 		const Animal	*j = new Dog();
 		const Animal	*i = new Cat();
 
+		std::cout << "\n\n";
 		std::cout << j->getType() << " " << std::endl;
 		std::cout << i->getType() << " " << std::endl;
-		i->makeSound(); //will output the cat sound!
+		i->makeSound();
 		j->makeSound();
 		meta->makeSound();	
+		std::cout << "\n\n";
 
 		delete meta;
 		delete j;
 		delete i;
+	}
+	std::cout << "\n\n";
+	{
+		std::cout << "TEST 0" << std::endl;
+		Cat kitty;
+		Animal *animal = &kitty;
+
+		std::cout << "\n\n";
+		animal->makeSound();
+		std::cout << "\n\n";
+	}
+	std::cout << "\n\n";
+	{
+		std::cout << "TEST 0" << std::endl;
+		WrongCat wrongCat;
+		WrongAnimal *wrongAnimal = &wrongCat;
+
+		std::cout << "\n\n";
+		wrongAnimal->makeSound();
+		std::cout << "\n\n";
 	}
 	return (0);
 
