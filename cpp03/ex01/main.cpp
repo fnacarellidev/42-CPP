2958fed07169f68c811a13ccf4d6e78e07c6928c
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-02-01 17:07:04 -0300

    some tests for ex01 cpp03

--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -1,5 +1,34 @@
 #include "ScavTrap.hpp"
 
 int main() {
+	{
+		std::cout << "TEST 0" << std::endl;
+		ClapTrap defaultClap;
+
+		defaultClap.attack("clock");
+		defaultClap.beRepaired(10);
+		defaultClap.printInfo();
+		defaultClap.beRepaired(10);
+		defaultClap.beRepaired(10);
+		defaultClap.printInfo();
+		defaultClap.takeDamage(40);
+		defaultClap.printInfo();
+		defaultClap.takeDamage(40);
+	}
+	{
+		std::cout << "TEST 1" << std::endl;
+		ScavTrap	riny("fabin");
+
+		riny.attack("clock");
+		riny.guardGate();
+		riny.printInfo();
+		riny.beRepaired(50);
+		riny.printInfo();
+		riny.takeDamage(50);
+		riny.printInfo();
+		riny.takeDamage(100);
+		riny.printInfo();
+		riny.guardGate();
+	}
 	return (0);
 }
