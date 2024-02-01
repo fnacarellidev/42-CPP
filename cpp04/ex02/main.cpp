ed684420f5d54e01ced6460c0bf090fa650b5b0b
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-02-01 20:18:14 -0300

    abstract class little test

--- cpp04/ex02/main.cpp
+++ cpp04/ex02/main.cpp
@@ -3,27 +3,31 @@
 #include "Dog.hpp"
 
 int main() {
-	Dog		*new_doggo = new Dog();
-	Dog		*new_doggo2 = new Dog(*new_doggo);
-	AAnimal **animals = new AAnimal*[100];
+	/* Aanimal abstractAnimal = Aanimal(); */ // won't compile
 
-	for (int i = 0; i < 50; ++i)
-		animals[i] = new Cat();
-	for (int i = 50; i < 100; ++i)
-		animals[i] = new Dog();
+	{
+		Dog		*new_doggo = new Dog();
+		Dog		*new_doggo2 = new Dog(*new_doggo);
+		AAnimal **animals = new AAnimal*[100];
 
-	for (int i = 0; i < 100; ++i) {
-		std::cout << animals[i]->getType() << std::endl;
-		delete animals[i];
+		for (int i = 0; i < 50; ++i)
+			animals[i] = new Cat();
+		for (int i = 50; i < 100; ++i)
+			animals[i] = new Dog();
+
+		for (int i = 0; i < 100; ++i) {
+			std::cout << animals[i]->getType() << std::endl;
+			delete animals[i];
+		}
+
+		new_doggo->setIdea(0, "chimkem numgets");
+		std::cout << new_doggo->getIdea(0) << std::endl;
+		std::cout << (void*) new_doggo << std::endl;
+		std::cout << (void*) new_doggo2 << std::endl;
+
+		delete new_doggo;
+		delete new_doggo2;
+		delete[] animals;
 	}
-
-	new_doggo->setIdea(0, "chimkem numgets");
-	std::cout << new_doggo->getIdea(0) << std::endl;
-	std::cout << (void*) new_doggo << std::endl;
-	std::cout << (void*) new_doggo2 << std::endl;
-
-	delete new_doggo;
-	delete new_doggo2;
-	delete[] animals;
 	return (0);
 }
