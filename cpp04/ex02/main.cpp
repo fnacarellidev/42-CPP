307308d3123ead9954b2b8124702b366cc6f7102
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-02-06 16:09:03 -0300

    fixing class name, also erased get type from test

--- cpp04/ex02/main.cpp
+++ cpp04/ex02/main.cpp
@@ -3,7 +3,7 @@
 #include "Dog.hpp"
 
 int main() {
-	/* Aanimal abstractAnimal = Aanimal(); */ // won't compile
+	// AAnimal abstractAnimal = Aanimal(); // won't compile
 
 	{
 		Dog		*new_doggo = new Dog();
@@ -15,10 +15,8 @@ int main() {
 		for (int i = 50; i < 100; ++i)
 			animals[i] = new Dog();
 
-		for (int i = 0; i < 100; ++i) {
-			std::cout << animals[i]->getType() << std::endl;
+		for (int i = 0; i < 100; ++i)
 			delete animals[i];
-		}
 
 		new_doggo->setIdea(0, "chimkem numgets");
 		std::cout << new_doggo->getIdea(0) << std::endl;
