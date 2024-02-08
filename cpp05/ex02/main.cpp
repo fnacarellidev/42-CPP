53ffb400e698df660f126a7bb9608671b5f8f06c
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-02-08 18:56:29 -0300

    feat: new tests cpp05 ex02

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -1,29 +1,65 @@
 #include "Bureaucrat.hpp"
 #include "AForm.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
 
 int main() {
 	{
-		ShrubberyCreationForm test1("okaoka");
-		Bureaucrat		failsToExecute("rand", 144);
+		std::cout << "TEST 0\n";
+		Bureaucrat				ceo("ceo", 1);
+		ShrubberyCreationForm	shrub("shrub");
+		RobotomyRequestForm		robotomy("robotomy");
+		PresidentialPardonForm	presidential("presidential");
 
-		failsToExecute.signForm(test1);
-		try {
-			test1.execute(failsToExecute);
-		} catch (std::exception &e) {
-			std::cout << e.what() << std::endl;
-		}
+		std::cout << ceo << std::endl;
+
+		ceo.signForm(shrub);
+		ceo.signForm(robotomy);
+		ceo.signForm(presidential);
+		std::cout << std::endl;
+		std::cout << shrub << std::endl;
+		std::cout << robotomy << std::endl;
+		std::cout << presidential << std::endl;
+		std::cout << std::endl;
+		ceo.executeForm(shrub);
+		std::cout << std::endl;
+		ceo.executeForm(robotomy);
+		std::cout << std::endl;
+		ceo.executeForm(presidential);
 	}
 	{
-		ShrubberyCreationForm test1("okaoka");
-		Bureaucrat		succeedsToExecute("rand", 1);
+		std::cout << "TEST 1\n";
+		
+		Bureaucrat				ceo("ceo", 1);
+		Bureaucrat				intern("intern", 150);
+		ShrubberyCreationForm	shrub("shrub");
+		RobotomyRequestForm		robotomy("robotomy");
+		PresidentialPardonForm	presidential("presidential");
 
-		succeedsToExecute.signForm(test1);
-		try {
-			test1.execute(succeedsToExecute);
-		} catch (std::exception &e) {
-			std::cout << e.what() << std::endl;
-		}
+		std::cout << ceo << std::endl;
+		std::cout << intern << std::endl;
+		std::cout << std::endl;
+
+		intern.executeForm(shrub);
+		intern.executeForm(robotomy);
+		intern.executeForm(presidential);
+		std::cout << std::endl;
+		intern.signForm(shrub);
+		intern.signForm(robotomy);
+		intern.signForm(presidential);
+		std::cout << std::endl;
+		intern.executeForm(shrub);
+		intern.executeForm(robotomy);
+		intern.executeForm(presidential);
+		std::cout << std::endl;
+		ceo.signForm(shrub);
+		ceo.signForm(robotomy);
+		ceo.signForm(presidential);
+		std::cout << std::endl;
+		intern.executeForm(shrub);
+		intern.executeForm(robotomy);
+		intern.executeForm(presidential);
 	}
 	return 0;
 }
