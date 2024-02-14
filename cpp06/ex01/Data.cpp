0a591209c7d1457cb58596ec5eb6463467f37925
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-02-14 17:38:44 -0300

    feat: ifdef debug

--- cpp06/ex01/Data.cpp
+++ cpp06/ex01/Data.cpp
@@ -1,19 +1,27 @@
 #include "Data.hpp"
 
 Data::~Data() {
+#ifdef DEBUG
 	std::cout << "[DATA] Destructor called" << '\n';
+#endif
 }
 
 Data::Data() {
+#ifdef DEBUG
 	std::cout << "[DATA] Default constructor called" << '\n';
+#endif
 }
 
 Data::Data(int num) : x(num) {
+#ifdef DEBUG
 	std::cout << "[DATA] Parameter based constructor called" << '\n';
+#endif
 }
 
 Data::Data(const Data& copy) {
+#ifdef DEBUG
 	std::cout << "[DATA] Copy constructor called" << '\n';
+#endif
 	if (this != &copy) *this = copy;
 }
 
