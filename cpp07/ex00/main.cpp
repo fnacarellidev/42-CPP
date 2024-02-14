282c15d6f0c74e2f6682ce0ca9931603ea495454
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-02-14 18:27:15 -0300

    feat: new test cpp07 ex00

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -2,18 +2,31 @@
 #include <iostream>
 
 int main(void) {
-	int a = 2;
-	int b = 3;
-	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-	std::string c = "chaine1";
-	std::string d = "chaine2";
-	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	{
+		std::cout << "TEST 0\n";
+		int a = 2;
+		int b = 3;
+		::swap( a, b );
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		std::string c = "chaine1";
+		std::string d = "chaine2";
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	}
+	std::cout << "\n";
+	{
+		std::cout << "TEST 1\n";
+		int ten = 10;
+		int twenty = 20;
+
+		::swap(ten, twenty);
+		std::cout << "ten: " << ten << std::endl;
+		std::cout << "twenty: " << twenty << std::endl;
+	}
 	return 0;
 }
 
