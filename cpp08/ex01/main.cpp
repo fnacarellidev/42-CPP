821edae2470ed0dda3e1138e00e64f5073f7dced
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-02-28 16:44:26 -0300

    feat: adding subject test

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -1,22 +1,39 @@
 #include "Span.hpp"
 
 int main() {
-	Span sp(20);
-	std::vector<int> x;
+	{
+		std::cout << "TEST 1\n";
+		Span sp(20);
+		std::vector<int> x;
 
-	x.push_back(10);
-	x.push_back(12);
-	x.push_back(32);
-	x.push_back(40);
-	sp.addNumber(6);
-	sp.addNumber(3);
-	sp.addNumber(17);
-	sp.addNumber(9);
-	sp.addNumber(11);
-	sp.addNumber(x.begin(), x.end());
+		x.push_back(10);
+		x.push_back(12);
+		x.push_back(32);
+		x.push_back(40);
+		sp.addNumber(6);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+		sp.addNumber(x.begin(), x.end());
 
-	std::cout << sp.shortestSpan() << '\n';
-	std::cout << sp.longestSpan() << '\n';
+		std::cout << sp.shortestSpan() << '\n';
+		std::cout << sp.longestSpan() << '\n';
+	}
+	std::cout << "\n";
+	{
+		std::cout << "TEST 2 (SUBJECT)\n";
+		Span sp = Span(5);
+
+		sp.addNumber(6);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+	}
 
 	return 0;
 }
