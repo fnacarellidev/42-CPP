2328596b3e82f62c7a94429b42cbf9dd43129689
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-02-28 16:27:13 -0300

    feat: adding test for vector

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -1,18 +1,33 @@
 #include "easyfind.hpp"
 #include <list>
+#include <vector>
 
 int main() {
 	{
-		std::cout << "TEST 1" << '\n';
+		std::cout << "TEST 1 LIST<INT>\n";
 		std::list<int> array;
 
 		for (int i = 0; i <= 100; ++i)
 			array.push_back(i);
-		::easyfind(array, 1000);
-		::easyfind(array, 1);
-		::easyfind(array, 2000);
-		::easyfind(array, 42);
-		::easyfind(array, 22);
-		::easyfind(array, 132);
+		easyfind(array, 1000);
+		easyfind(array, 1);
+		easyfind(array, 2000);
+		easyfind(array, 42);
+		easyfind(array, 22);
+		easyfind(array, 132);
+	}
+	std::cout << "\n";
+	{
+		std::cout << "TEST 2 VECTOR<INT>\n";
+		std::vector<int> vector;
+
+		for (int i = 0; i <= 10000; ++i)
+			vector.push_back(i);
+		easyfind(vector, 1);
+		easyfind(vector, 2);
+		easyfind(vector, 42);
+		easyfind(vector, 10001);
+		easyfind(vector, 20000);
+		easyfind(vector, 42000);
 	}
 }
